2d184000962285ca1097c5fa0eb8e2d20430ad78
franckzzz <mateus.franck@unioeste.br>
2024-07-08 23:31:54 -0300

    Update 1042.c

--- 1042.c
+++ 1042.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+int main(){
+  char filo[15], classe[15], ordem[15];
+  scanf("%s", filo);
+  if(strcmp(filo,"vertebrado")==0){
+    scanf("%s", classe);
+    if (strcmp(classe,"ave")==0){
+      scanf("%s", ordem);
+      if (strcmp(ordem,"carnivoro")==0){
+        printf("aguia\n");
+      } else if (strcmp(ordem,"onivoro")==0){
+        printf("pomba\n");
+      }
+    } else if(strcmp(classe,"mamifero")==0){
+      scanf("%s", ordem);
+      if (strcmp(ordem,"onivoro")==0){
+        printf("homem\n");
+      } else if (strcmp(ordem,"herbivoro")==0){
+        printf("vaca\n");
+      }
+    }
+  } else if(strcmp(filo,"invertebrado")==0){
+      scanf("%s", classe);
+      if(strcmp(classe,"inseto")==0){
+        scanf("%s", ordem);  
+        if(strcmp(ordem,"hematofago")==0){
+          printf("pulga\n");
+        } else if(strcmp(ordem,"herbivoro")==0){
+          printf("lagarta\n");
+        }
+      } else if (strcmp(classe,"anelideo")==0){
+        scanf("%s", &ordem);  
+        if(strcmp(ordem,"hematofago")==0){
+          printf("sanguessuga\n");
+        } else if(strcmp(ordem,"onivoro")==0){
+          printf("minhoca\n");
+        }
+      }
+  }
+
+  return 0;
+}
