2470d63f48b7bb4cdc9d4f51dce8f97a369b658c
franckzzz <mateus.franck@unioeste.br>
2024-08-01 12:29:25 -0300

    Create 1118.c

---
+++ 1118.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+    double x, y;
+    int n, a;
+    do{
+        while(scanf("%lf", &x) && (x >10.0 || x <0.0)){
+            printf("nota invalida\n");
+            x=0;
+        }
+        while(scanf("%lf", &y) && (y >10.0 || y <0.0)){
+            printf("nota invalida\n");
+            y=0;
+        }
+        printf("media = %.2lf\n", (x+y)/2);
+        printf("novo calculo (1-sim 2-nao)\n");
+        while(scanf("%d", &a) && (a>2 || a<1)){
+            printf("novo calculo (1-sim 2-nao)\n");
+        }
+        if (a==2){
+            return 0;
+        }
+    }
+    while(a==1);
+    return 0;
+}
